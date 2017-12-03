f3ea466e8e999fca58edaa89e4ce8f71de86b7db
Jin Park <jiny199109@gmail.com>
2017-12-03 16:14:33 +0900

    Done to Ch.12

--- Source/BatteryCollector/BatteryCollectorCharacter.cpp
+++ Source/BatteryCollector/BatteryCollectorCharacter.cpp
@@ -10,6 +10,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include <Runtime/Engine/Classes/Components/SphereComponent.h>
 #include "Pickup.h"
+#include "BatteryPickup.h"
 
 //////////////////////////////////////////////////////////////////////////
 // ABatteryCollectorCharacter
@@ -152,6 +153,9 @@ void ABatteryCollectorCharacter::CollectPickups()
 	TArray<AActor*> CollectedActors;
 	CollectionSphere->GetOverlappingActors(CollectedActors);
 
+	// Keep track of the collected battery power
+	float CollectedPower = 0;
+
 	// For each Actor we collected
 	for (int32 iCollected = 0; iCollected < CollectedActors.Num(); iCollected++)
 	{
@@ -163,10 +167,23 @@ void ABatteryCollectorCharacter::CollectPickups()
 		{			
 			// Call the pickup's WasCollected function
 			TestPickup->WasCollected();
+			
+			// Check to see if the pickup is also a battery
+			ABatteryPickup* const TestBattery = Cast<ABatteryPickup>(TestPickup);
+			if (TestBattery)
+			{
+				// increase the collected power
+				CollectedPower += TestBattery->GetPower();
+			}
+
 			// Deactivate the pickup
 			TestPickup->SetActive(false);
 		}
 	} 
+	if (CollectedPower > 0)
+	{
+		UpdatePower(CollectedPower);
+	}
 }
 
 // Reports starting power
